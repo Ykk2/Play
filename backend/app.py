d6dd576419669fdf57ecff626fab1875e55d9696
Richard <richardkwon5@gmail.com>
2023-03-28 19:01:40 -0700

    adding to gitignore

---
+++ backend/app.py
@@ -0,0 +1,17 @@
+from flask import Flask
+from flask_cors import CORS
+from flask_login import LoginManager
+
+
+
+app = Flask(__name__, static_folder="../frontend/.next", static_url_path='/')
+
+CORS(app)
+
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.get(user_id)
