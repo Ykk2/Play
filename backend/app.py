ca91af690683bbb84f4f5a74448dae59052acf98
Richard <richardkwon5@gmail.com>
2023-03-31 01:39:37 -0700

    saving

--- backend/app.py
+++ backend/app.py
@@ -1,9 +1,7 @@
 from flask import Flask
 from flask_cors import CORS
 from flask_login import LoginManager
-from .models import db
-
-
+from .models import db, User
 
 app = Flask(__name__, static_folder="../frontend/.next", static_url_path='/')
 
@@ -11,10 +9,17 @@ CORS(app)
 
 db.init_app(app)
 
+
+
+
+
+
+
+
+
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 
-
 @login_manager.user_loader
 def load_user(user_id):
-    return User.get(user_id)
+    return User.query.get(int(user_id))
