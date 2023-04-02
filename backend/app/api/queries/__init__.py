ba5bf75d70ad19d7949a495197d65bacd9b82f23
Richard <richardkwon5@gmail.com>
2023-04-02 00:45:10 -0700

    add imports to __init__.py at api > queries

--- backend/app/api/queries/__init__.py
+++ backend/app/api/queries/__init__.py
@@ -1,4 +1,4 @@
 from .question_queries import *
-from concept_queries import *
-from bookmark_queries import *
-from complaints_queries import *
+from .concept_queries import *
+from .bookmark_queries import *
+from .complaints_queries import *
