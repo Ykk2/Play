685aae21a52faec13cb5fed70b44cb54312bce38
Richard <richardkwon5@gmail.com>
2023-04-01 19:01:05 -0700

    changed app configuration so server starts as expected

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,17 +1,21 @@
 from flask import Flask
 from flask_cors import CORS
 from flask_login import LoginManager
+from .models import db, User
+from .config import Config
 
 
 app = Flask(__name__, static_folder="../frontend/.next", static_url_path='/')
-
+app.config.from_object(Config)
 
 CORS(app)
 
-
-
-
-
+db.init_app(app)
 
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.get(user_id)
